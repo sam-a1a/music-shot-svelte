5b80ee1d5c0a6c0dfb73cde8a4497d1d6c7eb573
Sam <BassamGhazaleh@proton.me>
2026-05-18 01:27:22 +0300

    init: scaffold sveltekit project with tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,5 @@
-import { fileURLToPath, URL } from 'node:url'
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import { paraglideVitePlugin } from '@inlang/paraglide-js'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    paraglideVitePlugin({
-      project: './project.inlang',
-      outdir: './src/paraglide',
-    }),
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-  server: {
-    port: 3300
-  }
-})
+export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
